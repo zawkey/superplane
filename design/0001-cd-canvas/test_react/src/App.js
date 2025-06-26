ba3fc88e341248aab37f648ad7b02e26d6f7dab9
Zorana Mitrović Vučičević <zorana@Zoranas-MacBook-Air.local>
2025-06-26 15:33:26 +0200

    login page added

--- design/0001-cd-canvas/test_react/src/App.js
+++ design/0001-cd-canvas/test_react/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import './App.css';
 import WorkflowEditor from './WorkflowEditor';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="flex flex-col h-screen font-sans">
-      <main className="flex-grow flex flex-col">
-        <WorkflowEditor />
-      </main>
-    </div>
+    <Router>
+      <div className="flex flex-col h-screen font-sans">
+        <Routes>
+          <Route path="/login" element={
+            <main className="flex-grow flex flex-col pa4 bg-white">
+              <Login />
+            </main>
+            } />
+          <Route 
+            path="/workflow" 
+            element={
+              <main className="flex-grow flex flex-col">
+                <WorkflowEditor />
+              </main>
+            }
+          />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ design/0001-cd-canvas/test_react/src/components/Login.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import githubLogo from "../images/icn-github.svg";
+import bitbucketLogo from "../images/icn-bitbucket.svg";
+import semaphoreLogo from "../images/semaphore-logo-sign-black.svg";
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate('/workflow');
+  };
+
+  return (
+    <div className="login-wrapper h-full flex flex-col items-center bg-washed-gray br3 justify-start pt6 px-4 sm:px-6 lg:px-8">
+      <div className="flex items-center mb5">
+        <img src={semaphoreLogo} alt="Semaphore Logo" className="h-6" width={26} /> <spanc className="ml2 f2 b">SuperPlane</spanc></div>
+      <div className="max-w-md w-full bg-white pa4 br3 shadow-sm ba b--black-10">
+        <div>
+          <h3 className="text-center b dark-gray">
+            Log in to SuperPlane
+          </h3>
+        </div>
+        <form className="mt-8" onSubmit={handleSubmit}>
+          <div className="rounded-md shadow-sm mb-6">
+            <div className='mb4'>
+            <button type="submit" className="bg-near-white btn btn-secondary btn-large w-full text-center inline-flex items-center justify-center">
+                <img src={githubLogo} className="mr2 flex-shrink-0"/>
+                Continue <span className="ml1"> with GitHub</span>
+            </button>
+            </div>
+            <div className="mb4 hidden">
+            <button type="submit" className="bg-near-white btn btn-secondary btn-large w-full text-center inline-flex items-center justify-center">
+                <img src={bitbucketLogo} className="mr2 flex-shrink-0"/>
+                Continue <span className="ml1"> with Bitbucket</span>
+            </button>
+            </div>
+          </div>
+
+          <div className=''>
+          <p className="text-center gray text-xs">By continuing, you agree to our <a className='link dark-indigo hover:underline' href="https://semaphoreci.com/tos">Terms of Service</a> and <a className='link dark-indigo hover:underline' href="https://semaphoreci.com/privacy">Privacy policy</a>.</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
